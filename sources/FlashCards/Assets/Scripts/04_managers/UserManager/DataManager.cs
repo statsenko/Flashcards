e51d20388dd1c9fa82dedb8fa751c87c1e147202
statsenko <statsenko@MacBook-Pro-statsenko.local>
2014-06-07 16:22:21 +0300

    initial project

---
+++ sources/FlashCards/Assets/Scripts/04_managers/UserManager/DataManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DataManager : MonoBehaviour
+{	
+	protected static DataManager mInstance = null;
+	public static DataManager Instance 
+	{
+		get 
+		{
+			if (mInstance == null)
+			{
+				GameObject go = new GameObject("_DataManager");
+				mInstance = go.AddComponent(typeof(DataManager)) as DataManager;	
+				DontDestroyOnLoad(go);
+			}
+			return mInstance;
+		}
+	}
+	
+	void OnDestroy()
+	{
+		mInstance = null;
+	}
+
+	public static DataManager GetInstance()
+	{
+		return Instance;
+	}
+
+	
+}
---
+++ sources/FlashCards/Assets/Scripts/05_UIControllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginController : UIController
+{
+	private DataManager _dataManager;
+
+	
+	void Awake()
+	{
+		DoAwake ();
+	}
+
+	void Start()
+	{
+		DoStart();
+		_dataManager = DataManager.Instance;
+		Debug.LogWarning("DataManager loaded");
+	}
+
+
+
+
+}
