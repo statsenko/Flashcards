a54189586ea6afc7411c1a8e01d9260fbafd9212
statsenko <statsenko@MacBook-Pro-statsenko.local>
2014-06-08 18:22:24 +0300

    DecksScreen

---
+++ sources/FlashCards/Assets/Scripts/05_UIControllers/DeckSceneController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeckSceneController : UIController
+{
+	private DataManager _dataManager;
+
+	
+	void Awake()
+	{
+		DoAwake ();
+	}
+
+	void Start()
+	{
+		DoStart();
+		_dataManager = DataManager.Instance;
+		Debug.LogWarning("DataManager loaded");
+	}
+
+	void OnBackButton (GameObject sender)
+	{
+		Debug.LogWarning("On Back Button");
+
+
+	}
+
+	void OnSignInOrSynchronize(GameObject sender)
+	{
+		Debug.LogWarning("OnSignInOrSynchronize");
+	}
+}
